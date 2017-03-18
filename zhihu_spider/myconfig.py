c3f6db2d4e3c4a572b6a5650ee585c408589fe20
Double X Li <bajnok33@gmail.com>
2017-03-18 22:59:32 +0800

    add myconfig.py

---
+++ zhihu_spider/myconfig.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+UsersConfig = {
+    # 代理
+    'proxy': '',
+
+    # 知乎用户名和密码
+    'email': 'your_email',
+    'password': 'your_password',
+}
+
+DbConfig = {
+    # db config
+    'user': 'db user',
+    'passwd': 'db password',
+    'db': 'db name',
+    'host': 'db host',
+}
+
+PhantomJSConfig = {
+    'path': r'D:\Program Files (x86)\GreenSoftware\phantomjs-2.1.1-windows\bin\phantomjs.exe'
+}
